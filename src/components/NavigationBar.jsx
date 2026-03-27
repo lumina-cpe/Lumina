40b900c83b9124005620a9b0217e2251f62e75b9
ailo <tomateliannielz@gmail.com>
2026-03-27 22:53:15 +0800

    <feat>(components, states): glued components making up the Menu State

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,7 +5,7 @@ import ProgressBar from "./ProgressBar"
 export default function NavigationBar({ logoPath, title, progress })
 {
     return (<>
-        <image src={logoPath} alt="Logo" />
+        <img src={logoPath} alt="Logo" />
         <h1>{title}</h1>
         <ProgressBar value={progress} />
     </>);
--- src/states/MenuState.jsx
+++ src/states/MenuState.jsx
@@ -1,8 +1,13 @@
+import "../../styles/MenuState.css";
+
+import NavigationBar from "../components/NavigationBar";
 import SideBar from "../components/SideBar";
 
 export default function MenuState()
 {
     // CODE HERE
     return (<>
+        <NavigationBar logoPath={"../../assets/images/logo.png"} title={"Lumina: Shedding Light to the World of Computer Engineering"} progress={60}/>
+        <SideBar />
     </>);
 }
