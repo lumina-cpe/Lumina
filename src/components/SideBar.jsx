8e3b493cbe17043f09c37b81f6fc0950813158bc
ailo <tomateliannielz@gmail.com>
2026-03-28 11:21:43 +0800

    <ref>(components): changed expected icon for socials button from png format to svg

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,10 +10,10 @@ export default function SideBar() {
             <TextButton text="Settings"      callback={() => {}} />
             <TextButton text="More Settings" callback={() => {}} />
 
-        <IconButton imagePath="../../assets/images/github_icon.png" callback={() => {
+        <IconButton imagePath="../../assets/svgs/github_icon.svg" callback={() => {
             window.open("https://github.com/liantomate/web_design_cpe_olympiad_2026", "_blank");
         }} />
-        <IconButton imagePath="../../assets/images/wikipedia_icon.png" callback={() => {
+        <IconButton imagePath="../../assets/svgs/wikipedia_icon.svg" callback={() => {
             window.open("https://en.wikipedia.org/wiki/Computer_engineering", "_blank");
         }}/>
         </div>
