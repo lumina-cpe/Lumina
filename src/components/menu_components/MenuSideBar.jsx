365eb9bf8e9588e7ce5bd0fcc870df8c880795c8
runner-cpp <santosrhanner@gmail.com>
2026-03-30 23:41:43 +0800

    <fix>(Theme Button): Remove emoji. Text now centered.

--- src/components/menu_components/MenuSideBar.jsx
+++ src/components/menu_components/MenuSideBar.jsx
@@ -39,7 +39,7 @@ export default function SideBar() {
 
     return (
       <TextButton
-        text={theme === "dark" ? "Dark 🌙" : "Light ☀️"}
+        text={theme === "dark" ? "Dark" : "Light"}
         callback={toggleTheme}
         toggled={theme === "dark"}
       />
